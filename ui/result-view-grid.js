2bc9944dada64a133bebe6c3db630e5f96297244
Mark Simmons <msimmonz@gmail.com>
2018-05-23 22:41:17 -0400

    Fix location of sql statement when present

--- ui/result-view-grid.js
+++ ui/result-view-grid.js
@@ -8,47 +8,48 @@ const options = {
 var vm = new Vue({
     el: '#result-control',
     template: `
-    <div class="container-fluid">
-    <div v-if="hasError">
-      <div class="alert alert-danger" role="alert">{{sqlStatement.error}}</div>
-      <pre>{{sqlStatement.sql}}</pre>
+<div class="container-fluid">
+  <div v-if="hasError">
+    <div class="alert alert-danger" role="alert">{{sqlStatement.error}}</div>
+    <pre>{{sqlStatement.sql}}</pre>
+  </div>
+  <div class="row" v-if="!hasError">
+    <div class="col" v-if="isQuery && !busy">
+      <span class="badge badge-light">Executions &nbsp;<span class="badge badge-secondary">{{sqlStatement.executionCount}}</span></span>
+      <span class="badge badge-light">Elapsed &nbsp;<span class="badge badge-secondary">{{sqlStatement.executionTime}}</span></span>
+      <span class="badge badge-light">Rows &nbsp;<span class="badge badge-secondary">{{totalRows}}</span></span>
+      <span class="badge badge-light">More &nbsp;<span class="badge badge-secondary">{{sqlStatement.moreRows}}</span></span>
     </div>
-    <div class="row" v-if="!hasError">
-      <div class="col" v-if="isQuery && !busy">
-        <span class="badge badge-light">Executions &nbsp;<span class="badge badge-secondary">{{sqlStatement.executionCount}}</span></span>
-        <span class="badge badge-light">Elapsed &nbsp;<span class="badge badge-secondary">{{sqlStatement.executionTime}}</span></span>
-        <span class="badge badge-light">Rows &nbsp;<span class="badge badge-secondary">{{totalRows}}</span></span>
-        <span class="badge badge-light">More &nbsp;<span class="badge badge-secondary">{{sqlStatement.moreRows}}</span></span>
+    <div class="col" v-if="!isQuery && !busy">
+      <span class="badge badge-light">Elapsed &nbsp;<span class="badge badge-secondary">{{sqlStatement.executionTime}}</span></span>
+      <span class="badge badge-light">Rows Affected &nbsp;<span class="badge badge-secondary">{{sqlStatement.updateCount}}</span></span>
+      <span class="badge badge-light">Status &nbsp;<span class="badge badge-secondary">{{sqlStatement.status}}</span></span>
+    </div>
+    <div class="col" v-if="busy">
+      <span class="fa fa-spinner fa-pulse fa-3x fa-fw"></span>
+      <span class="badge">Executing</span>
+    </div>
+    <div class="col">
+      <div class="btn-group" role="group">
+        <button :disabled="busy" class="btn btn-success btn-sm fa fa-refresh" title="Refresh" @click="execute('reexecute')"/>
+        <button :disabled="!busy" class="btn btn-danger btn-sm fa fa-stop" title="Cancel" @click="execute('cancel')"/>
       </div>
-      <div class="col" v-if="!isQuery && !busy">
-        <span class="badge badge-light">Elapsed &nbsp;<span class="badge badge-secondary">{{sqlStatement.executionTime}}</span></span>
-        <span class="badge badge-light">Rows Affected &nbsp;<span class="badge badge-secondary">{{sqlStatement.updateCount}}</span></span>
-        <span class="badge badge-light">Status &nbsp;<span class="badge badge-secondary">{{sqlStatement.status}}</span></span>
-        <pre>{{sqlStatement.sql}}</pre>
+      <div class="btn-group" role="group">
+        <button :disabled="!isTxn" class="btn btn-success btn-sm fa fa-check" title="Commit" @click="execute('commit')"/>
+        <button :disabled="!isTxn" class="btn btn-warning btn-sm fa fa-undo" title="Rollback" @click="execute('rollback')"/>
       </div>
-      <div class="col" v-if="busy">
-        <span class="fa fa-spinner fa-pulse fa-3x fa-fw"></span>
-        <span class="badge">Executing</span>
-        <pre>{{sqlStatement.sql}}</pre>
-      </div>
-      <div class="col">
-        <div class="btn-group" role="group">
-          <button :disabled="busy" class="btn btn-success btn-sm fa fa-refresh" title="Refresh" @click="execute('reexecute')"/>
-          <button :disabled="!busy" class="btn btn-danger btn-sm fa fa-stop" title="Cancel" @click="execute('cancel')"/>
-        </div>
-        <div class="btn-group" role="group">
-          <button :disabled="!isTxn" class="btn btn-success btn-sm fa fa-check" title="Commit" @click="execute('commit')"/>
-          <button :disabled="!isTxn" class="btn btn-warning btn-sm fa fa-undo" title="Rollback" @click="execute('rollback')"/>
-        </div>
-        <button :disabled="!totalRows" class="btn btn-success btn-sm fa fa-download" title="Export" @click="execute('export')"/>
-        <button class="btn btn-danger btn-sm fa fa-close" title="Close" @click="execute('close')"/>
-      </div>
-      <div class="col">
-        <input class="form-control form-control-sm" type="text" placeholder="Filter..." style="width:100%;"/>
-      </div>
-      </div>
-      <div id="result-grid" />
-      </div>
+      <button :disabled="!totalRows" class="btn btn-success btn-sm fa fa-download" title="Export" @click="execute('export')"/>
+      <button class="btn btn-danger btn-sm fa fa-close" title="Close" @click="execute('close')"/>
+    </div>
+    <div class="col" v-if="isQuery && !busy">
+      <input class="form-control form-control-sm" type="text" placeholder="Filter..." style="width:100%;"/>
+    </div>
+  </div>
+  <div class="row" v-if="!hasError && !isQuery">
+    <pre>{{ sqlStatement.sql }}</pre>
+  </div>
+  <div id="result-grid" />
+</div>
 `,
     data: function () {
         return {
