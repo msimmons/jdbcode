35a571b5bca6528ce1787b832dea6538caf9c3d8
mark simmons <mark.simmons@marks-MacBook-Pro.local>
2019-11-01 14:24:27 -0400

    Catch exception and set error

--- server/src/main/kotlin/net/contrapt/jdbcode/service/SchemaDescriber.kt
+++ server/src/main/kotlin/net/contrapt/jdbcode/service/SchemaDescriber.kt
@@ -74,12 +74,16 @@ class SchemaDescriber {
                 val returnsValue: String = returnTypes[procedures.getInt("PROCEDURE_TYPE")]
                 schemaData.addObject(ProcedureData(ObjectOwner(schemaName, catalogName), name, ObjectType.procedure, returnsValue))
             }
+            schemaData.resolved = true
+            return schemaData
+        }
+        catch(e: Exception) {
+            schemaData.error = e.message
+            return schemaData
         }
         finally {
             connection.close()
         }
-        schemaData.resolved = true
-        return schemaData
     }
 
     fun describeObject(dataSource: DataSource, objectData: ObjectData) : ObjectData {
