3799d260a0accdc77bae6b679a55e7cbd61fea06
mark simmons <mark.simmons@marks-mbp.ef.com>
2019-11-27 13:58:42 -0500

    Playing with capturing symbols

--- server/src/main/kotlin/net/contrapt/jdbcode/service/SqlGrammar.kt
+++ server/src/main/kotlin/net/contrapt/jdbcode/service/SqlGrammar.kt
@@ -4,6 +4,7 @@ import com.github.h0tk3y.betterParse.combinators.*
 import com.github.h0tk3y.betterParse.grammar.Grammar
 import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import com.github.h0tk3y.betterParse.grammar.parser
+import com.github.h0tk3y.betterParse.lexer.TokenMatch
 import com.github.h0tk3y.betterParse.parser.Parser
 
 /**
@@ -19,7 +20,24 @@ import com.github.h0tk3y.betterParse.parser.Parser
    }
  }
  */
+data class Symbol(
+        val namespace: List<String>,
+        val name: String,
+        val start: Int,
+        val end: Int
+)
 object SqlGrammar : Grammar<String>() {
+
+    val symbols = mutableSetOf<Symbol>()
+
+    private fun addSymbol(terms: List<TokenMatch>) {
+        val nameTerm = terms.last()
+        val name = if (nameTerm.type == QID) nameTerm.text.replace("\"", "") else nameTerm.text
+        val namespace = terms.take(terms.size-1).map { if (it.type == QID) it.text.replace("\"", "") else it.text}
+        val start = terms.first().position
+        val end = nameTerm.position+nameTerm.text.length-1
+        symbols.add(Symbol(namespace, name, start, end))
+    }
 
     private fun keyword(token: String) = "${token}\\b".toRegex(RegexOption.IGNORE_CASE)
 
@@ -68,8 +86,8 @@ object SqlGrammar : Grammar<String>() {
     val ASTERISK by token("\\*")
 
     // RULES
-    val id : Parser<String> by (QID or ID).map { "${it.type.name}[${it.text}]" } // Optionally Quoted ID
-    val fqId : Parser<String> by (separated(id, DOT, false)).map { it.terms.joinToString { it } } // Fully Qualified ID
+    val id : Parser<TokenMatch> by (QID or ID) // Optionally Quoted ID
+    val fqId : Parser<String> by (separated(id, DOT, false)).map { addSymbol(it.terms); it.terms.last().text }
     val literal : Parser<String> by (NULL or TRUE or FALSE or STRING_LITERAL or NUMERIC_LITERAL).map { "${it.type.name}[${it.text}]" } // A literal
 
     val simpleExpression : Parser<String> by (fqId or literal)
@@ -81,24 +99,25 @@ object SqlGrammar : Grammar<String>() {
 
     //val selectExpression : Parser<String> by ()
 
-    val select : Parser<String> by (SELECT * zeroOrMore(fqId) * FROM * zeroOrMore(ID)).map {  it.t2.joinToString { it } }
+    val select : Parser<String> by (SELECT * zeroOrMore(valueExpression) * FROM * zeroOrMore(fqId)).map { it.t2.joinToString { it } }
     val insert : Parser<String> by (INSERT * INTO * ID * VALUES * zeroOrMore(ID)).map { "" }
 
-    val statement : Parser<String> by (SELECT * zeroOrMore(fqId) * FROM * zeroOrMore(ID)).map { it.t2.joinToString { it } }
+    val statement : Parser<String> by (SELECT * zeroOrMore(fqId) * FROM * zeroOrMore(fqId)).map { it.t2.joinToString { it } }
 
     val idOrLiteral : Parser<String> by oneOrMore(valueExpression).map { it.joinToString { it } }
 
-    override val rootParser: Parser<String> by idOrLiteral
+    override val rootParser: Parser<String> by (select or idOrLiteral)
 
     @JvmStatic
     fun main(args: Array<String>) {
         val sql = listOf(
-                "SelEct \"a\".\"blah\" from bar",
+                "SelEct \"a\".\"blah\", boo from bar",
                 "case id then 3 else 15 end",
                 "id over (partition by id order by id)",
                 "'This is a string''s literal' anid fe.id \"quoted id name\".id 'another literal' 9.0 9.99 .0109 1.01E-10 -1 -34E10 null '' concat(1,2,3,id.foo)",
-                "concat(1,32,1) substr(concat(12,232)) over (partition by foo.bar, max(1,2) order by 1, 2) case when 3 then 4 else 5 end"
+                "concat(\"baz\".\"biz\") substr(concat(12,232)) over (partition by foo.bar, max(1,2) order by 1, 2) case when 3 then 4 else 5 end"
         )
-        println(parseToEnd(sql[4]))
+        println(parseToEnd(sql[0]))
+        println(symbols)
     }
 }
